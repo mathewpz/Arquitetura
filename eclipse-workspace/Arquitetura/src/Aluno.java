86ab7f6226b6d60a9948d2adbaf93d078861e5e6
mathewpz <matheuspazine@gmail.com>
2020-03-10 00:39:48 -0300

    exercicio pronto

--- eclipse-workspace/Arquitetura/src/Aluno.java
+++ eclipse-workspace/Arquitetura/src/Aluno.java
@@ -1,8 +1,14 @@
 public class Aluno{
    private String nome;
    private int id;
+public String getNome() {
+	return nome;
+}
 public void setNome(String nome) {
 	this.nome = nome;
+}
+public int getId() {
+	return id;
 }
 public void setId(int id) {
 	this.id = id;
