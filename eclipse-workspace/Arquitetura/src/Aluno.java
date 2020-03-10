8e57e17e9e21c8c9af293e6d9fdd0fce7a3779ac
mathewpz <matheuspazine@gmail.com>
2020-03-10 00:28:50 -0300

    criacao de sets

--- eclipse-workspace/Arquitetura/src/Aluno.java
+++ eclipse-workspace/Arquitetura/src/Aluno.java
@@ -1,11 +1,11 @@
 public class Aluno{
    private String nome;
    private int id;
-public String getNome() {
-	return nome;
+public void setNome(String nome) {
+	this.nome = nome;
 }
-public int getId() {
-	return id;
+public void setId(int id) {
+	this.id = id;
 }
    
 }
